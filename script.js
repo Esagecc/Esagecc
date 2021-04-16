5046a5c3a7947d67bd6b454eea305c4d9ba651c1
Elizabeth C <32490614+Esagecc@users.noreply.github.com>
2021-04-15 23:01:45 -0500

    Adding the JavaScript and images
    Added the rest of the documents for mobile site.

---
+++ script.js
@@ -0,0 +1,40 @@
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+    if(!menuOpen) {
+        menuBtn.classList.add('open');
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove('open');
+        menuOpen = false;
+    }
+});
+
+function classToggle() {
+    const navs = document.querySelectorAll('.navbar__items')
+    
+    navs.forEach(nav => nav.classList.toggle('navbar__toggleshow'));
+  }
+  
+  document.querySelector('.navbar__link-toggle')
+    .addEventListener('click', classToggle);
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
